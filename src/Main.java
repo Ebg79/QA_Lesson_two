87431335320e32345564ebbd3392ae6893dda3fa
Evgeniy <ebg79@yandex.ru>
2023-04-27 14:16:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676;
+        int cost = 20;
+        int bonus = price / cost;
+        System.out.println(bonus);
+    }
+}
